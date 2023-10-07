9208c0527b48679606499a1fa1f0c62b7d3df513
CoshuLy <rhyu.agapito.miranda@gmail.com>
2023-10-08 02:18:38 +0800

    Add solution for 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = (firstNumber, secondNumber) => {
+  let sum = 0;
+  let userInput = true;
 
-};
+  if(firstNumber < 0 || secondNumber < 0){
+    return 'ERROR';
+  } else if (Number(firstNumber) !== firstNumber || Number(secondNumber) !== secondNumber){
+    return 'ERROR';
+  } else if (firstNumber > secondNumber){
+    for (let i = secondNumber; i <= firstNumber; i++){
+        sum += i;}
 
+        return sum;
+    } else if (firstNumber < secondNumber){
+        for (let i = firstNumber; i <= secondNumber; i++){
+            sum += i;}
+
+        return sum;
+    } else if (firstNumber !== Number || secondNumber !== Number){
+        return 'ERROR';
+    } 
+}
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let fahrenheitToCelsius = (fahrenheit - 32) * 5/9;
+  let roundCelsius = Number(fahrenheitToCelsius.toFixed(1));
+  return roundCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let celsiusToFahrenheit = 32 + (1.8 * celsius);
+  let roundFahrenheit = Number(celsiusToFahrenheit.toFixed(1));
+  return roundFahrenheit;
 };
 
 // Do not edit below this line
