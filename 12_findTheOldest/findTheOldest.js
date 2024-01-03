4dc5ccfd85d03146c10971f4edd5c3c48f86b71f
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-01-04 01:47:21 +0800

    Add solution for problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const currentYear = new Date().getFullYear();
+  
+  let arrMapped = arr.map((item, index) => {
+      let yearOfDeath = item.yearOfDeath || currentYear
+      return {
+          name: item.name,
+          age: yearOfDeath - item.yearOfBirth
+      }
+  });
+  
+  const oldestPerson = arrMapped.reduce((oldest, current) => {
+      return current.age > oldest.age ? current : oldest;
+  }, arrMapped[0]);
+  
+  return oldestPerson;
 };
-
+//npm test findTheOldest.spec.js
 // Do not edit below this line
 module.exports = findTheOldest;
