db0e0ddca64abd8eae187fa92e1d87d07729b024
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-01-03 22:08:56 +0800

    Add solution for problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  let numberParsed = parseInt(number);
+  const squareRoot = Math.sqrt(5);
+  const phi = (((1 + squareRoot) / 2) ** numberParsed);
+  const psi = (((1 - squareRoot) / 2) ** numberParsed);
+  
+  if (numberParsed < 0) {
+    return "OOPS";
+  }
+  
+  return Math.round(((phi - psi) / squareRoot));
 };
 
 // Do not edit below this line
