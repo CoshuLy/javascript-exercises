3dc98931fe541bb71c8e430c99663ada71cd71dd
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-01-03 16:15:27 +0800

    Add solution for problem 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = arr.reduce((sum, current) => sum + current, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr.reduce((product, current) => product * current, 1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(firstNumber, secondNumber) {
+
+  return firstNumber ** secondNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let product = 1;
+
+	for(let i = 1; i <= number; i++){
+      product *= i;
+    }
+
+  return product;
 };
 
 // Do not edit below this line
