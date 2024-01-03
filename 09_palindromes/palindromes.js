e834e5d39558b8af3f83d8bc13cfe1fc4cc71ec8
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-01-03 19:38:47 +0800

    Add solution for problem 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanString = str.replace(/[^a-z0-9]/gi, '');
 
-};
+    return cleanString.toLowerCase() === cleanString.split('').reverse().join('').toLowerCase();
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
